2a6162e4525437a132d9cf184aeec6aca6c70a82
Zhang Yimeng <saitoiyoli@sina.com>
2018-06-28 09:36:35 +0800

    20180628

--- src/test/java/test/UniqueTest.java
+++ src/test/java/test/UniqueTest.java
@@ -1,22 +1,13 @@
 package test;
 
-import java.util.Arrays;
+import org.bson.types.ObjectId;
 
-import org.bson.Document;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.wonder.mongodb.api.Duality;
 import com.wonder.mongodb.api.MongoDBConfigWithAuth;
 import com.wonder.mongodb.api.MongoDBPlayer;
 import com.wonder.mongodb.api.exception.IllegalPortValueException;
 import com.wonder.mongodb.api.exception.NoServerIPException;
 import com.wonder.mongodb.api.exception.SelectedCollectionWithNoIndexesException;
 import com.wonder.mongodb.api.foundation.MongoDBConfig;
-import com.wonder.mongodb.api.foundation.UpdateOperators;
 
 public class UniqueTest {
 
@@ -24,23 +15,10 @@ public class UniqueTest {
 	public static void main(String[] args) throws NoServerIPException, IllegalPortValueException, SelectedCollectionWithNoIndexesException {
 		String[] indexFields = {"a", "b", "c", "d"};
 		String[] uniqueFields = {"abcd"};
-		MongoDBConfig conf = new MongoDBConfigWithAuth("192.168.1.37", 27017, "mochi", "gotohellmyevilex", indexFields, uniqueFields);
+		MongoDBConfig conf = new MongoDBConfigWithAuth("127.0.0.1", 27017, "mochi", "gotohellmyevilex", indexFields, uniqueFields);
 		MongoDBPlayer player = new MongoDBPlayer(conf, "test", "DataInsertTest");
-		
-		ServerAddress server = new ServerAddress("192.168.1.37", 27017);
-		MongoCredential cred = MongoCredential.createCredential("mochi", "admin", "gotohellmyevilex".toCharArray());
-		@SuppressWarnings("resource")
-		MongoClient mc = new MongoClient(server, Arrays.asList(cred));
-		MongoDatabase db = mc.getDatabase("test");
-		MongoCollection<Document> collection = db.getCollection("DataInsertTest");
-		//"a" : "fb623001-9307-4561-9b1a-9023bb9c8270"
-		Duality filter = new Duality();
-		filter.append("a", "fb623001-9307-4561-9b1a-9023bb9c8270");
-		Duality value = new Duality();
-		value.append("b", "1234567");
-		Duality update = new Duality();
-		update.append(UpdateOperators.$SET, value);
-		player.findAndAlwaysUpdate(filter, update);
+		ObjectId oid = new ObjectId("5b31b008803cab1ba80b33e2");
+		System.out.println(oid.toString());
 	}
 
 }
